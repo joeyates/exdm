60a92481e9d799412345d79749128a852a914a88
Joe Yates <joe.g.yates@gmail.com>
2015-11-23 17:04:14 +0100

    Complete mix.exs
    * [ci skip]

--- mix.exs
+++ mix.exs
@@ -1,10 +1,15 @@
 defmodule Exdm.Mixfile do
   use Mix.Project
 
+  @version    "0.0.1"
+
   def project do
     [app: :exdm,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.0",
+     description: description,
+     package: package,
+     source_url: "https://github.com/joeyates/exdm",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -14,9 +19,21 @@ defmodule Exdm.Mixfile do
     [applications: [:logger]]
   end
 
+  defp description do
+    """
+    Deploy Elixir applications via mix tasks
+    """
+  end
+
+  defp package do
+    [licenses: ["MIT"],
+     maintainers: ["Joe Yates"],
+     links: %{github: "https://github.com/joeyates/exdm"}]
+  end
+
   defp deps do
     [
-      {:exrm, "0.19.9"},
+      {:exrm, "~> 0.19.9"},
       {:meck, "~> 0.8.3", only: :test}
     ]
   end
